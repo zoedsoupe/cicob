51f29ee76731bd161d16cad9ec729e36dab94141
Zoey de Souza Pessanha <zoey.pessanha@solfacil.com.br>
2022-08-22 13:06:26 -0300

    let domain_service as pure logic

--- lib/cicob/customer_data/services/create_email.ex
+++ lib/cicob/customer_data/services/create_email.ex
@@ -2,6 +2,7 @@ defmodule Cicob.CustomerData.Services.CreateEmail do
   use Cicob, :application_service
 
   alias Cicob.CustomerData.IO.CustomerEmailRepo
+  alias Cicob.CustomerData.IO.CustomerRepo
   alias Cicob.CustomerData.IO.EmailRepo
   alias Cicob.CustomerData.Models.CustomerEmail
   alias Cicob.CustomerData.Models.Email
@@ -10,8 +11,10 @@ defmodule Cicob.CustomerData.Services.CreateEmail do
   @impl true
   def process(params) do
     with %Email{} = email <- Email.new(params),
+         {:ok, customer} <- CustomerRepo.get(email.customer_id),
+         emails <- CustomerRepo.load_emails(customer) |> Map.get(:emails),
+         :ok <- EmailConstraint.validate(params, emails),
          {:ok, new_email} <- EmailRepo.insert(email),
-         :ok <- EmailConstraint.validate(params),
          %CustomerEmail{} = customer_email <-
            CustomerEmail.new(email_id: new_email.id, customer_id: email.customer_id),
          {:ok, _customer_email} <- CustomerEmailRepo.insert(customer_email) do
--- lib/cicob/customer_data/services/email_contraint.ex
+++ lib/cicob/customer_data/services/email_contraint.ex
@@ -1,32 +1,26 @@
 defmodule Cicob.CustomerData.Services.EmailConstraint do
   use Cicob, :domain_service
 
-  alias Cicob.CustomerData.IO.CustomerRepo
-
-  def validate(attrs, id \\ nil) do
-    with {:ok, :default} <- validate_default_rule(attrs, id),
-         {:ok, :active} <- validate_active_rule(attrs, id),
+  def validate(attrs, emails \\ [], id \\ nil) do
+    with {:ok, :default} <- validate_default_rule(attrs, emails, id),
+         {:ok, :active} <- validate_active_rule(attrs, emails, id),
          {:ok, :default_and_active} <- validate_active_and_default_rule(attrs) do
       :ok
     end
   end
 
-  defp validate_default_rule(%{default: false} = attrs, id) do
-    if has_default?(attrs.customer_id, id) do
+  defp validate_default_rule(%{default: false}, emails, id) do
+    if has_default?(emails, id) do
       {:ok, :default}
     else
       {:error, :at_least_one_default_email}
     end
   end
 
-  defp validate_default_rule(_attrs, nil), do: {:ok, :default}
+  defp validate_default_rule(_attrs, _emails, nil), do: {:ok, :default}
 
-  defp has_default?(customer_id, id) do
-    {:ok, customer} = CustomerRepo.get(customer_id)
-
-    customer
-    |> CustomerRepo.load_emails()
-    |> Map.get(:emails, [])
+  defp has_default?(emails, id) do
+    emails
     |> Enum.filter(& &1.default)
     |> case do
       [] -> false
@@ -34,12 +28,8 @@ defmodule Cicob.CustomerData.Services.EmailConstraint do
     end
   end
 
-  defp validate_active_rule(%{active: true} = attrs, nil) do
-    {:ok, customer} = CustomerRepo.get(attrs.customer_id)
-
-    customer
-    |> CustomerRepo.load_emails()
-    |> Map.get(:emails, [])
+  defp validate_active_rule(%{active: true}, emails, nil) do
+    emails
     |> Enum.filter(& &1.active)
     |> case do
       [] -> {:ok, :active}
@@ -47,22 +37,18 @@ defmodule Cicob.CustomerData.Services.EmailConstraint do
     end
   end
 
-  defp validate_active_rule(%{active: false} = attrs, id) do
-    if has_active?(attrs.customer_id, id) do
+  defp validate_active_rule(%{active: false}, emails, id) do
+    if has_active?(emails, id) do
       {:ok, :active}
     else
       {:error, :at_least_one_active_email}
     end
   end
 
-  defp validate_active_rule(_attrs, nil), do: {:ok, :active}
+  defp validate_active_rule(_attrs, _emails, nil), do: {:ok, :active}
 
-  defp has_active?(customer_id, id) do
-    {:ok, customer} = CustomerRepo.get(customer_id)
-
-    customer
-    |> CustomerRepo.load_emails()
-    |> Map.get(:emails, [])
+  defp has_active?(emails, id) do
+    emails
     |> Enum.filter(& &1.active)
     |> case do
       [] -> false
